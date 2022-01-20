0c7084d26b208737147a969a154a06ae8692513d
Michel DIEMER <michel.diemer@yahoo.fr>
2022-01-20 08:57:07 +0100

    dossier 15 renommé

--- 16 - MVC/mvc/footer.php
+++
@@ -1,7 +0,0 @@
-﻿            </div> <!-- #contenu -->
-            <footer id="piedBlog">
-                Blog réalisé avec PHP, HTML5 et CSS.
-            </footer>
-        </div> <!-- #global -->
-    </body>
-</html>
